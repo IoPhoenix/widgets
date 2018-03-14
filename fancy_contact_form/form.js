f77f7839538582627355fa9d4cf3e87ec3ffd2ed
IoPhoenix <olga.fomin26@gmail.com>
2018-03-13 23:51:59 -0700

    Added error messages for fields that were left empty

--- fancy_contact_form/form.js
+++ fancy_contact_form/form.js
@@ -1,4 +1,7 @@
-let inputs = document.querySelectorAll('.form-field input, .form-field textarea');
+const form = document.getElementsByTagName('form')[0],
+    inputs = document.querySelectorAll('.form-field input, .form-field textarea'),
+    errors = [ nameError, mailError, messageError] = document.getElementsByClassName('error');
+
 
 Array.from(inputs).forEach(i => {
     const parent = i.parentElement;
@@ -6,13 +9,30 @@ Array.from(inputs).forEach(i => {
     i.addEventListener('blur', () => {
         parent.classList.remove('active');
 
+        // select error field for current inout
+        const error = document.getElementById(`${i.name}-error`);
+
         // leave label text at the top of input if input was filled by user
         if (i.value !== '') {
-            parent.classList.remove('no-text');
-            parent.classList.add('has-text');
+            parent.classList.remove('empty');
+            parent.classList.add('filled');
         } else {
-            parent.classList.remove('has-text');
-            parent.classList.add('no-text');
+            parent.classList.remove('filled');
+            parent.classList.add('empty');
         }
+
+        // show error message if field was left empty
+        if (i.value === '') {
+            error.innerHTML = `Please enter your ${i.name}`;
+            error.className = "error active";
+        }
+
+        // remove error message if field was filled
+        i.addEventListener('input', () => {
+            if (i.value !== '') {
+                error.innerHTML = "";
+                error.classList.remove('active');
+            }
+        });
     });
 });
