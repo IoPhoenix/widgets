f2095568ec58a4fadb368685c409102d3d8c5a12
IoPhoenix <olga.fomin26@gmail.com>
2018-03-14 15:05:05 -0700

    Added email and message validation Added form validation on submit

--- fancy_contact_form/form.js
+++ fancy_contact_form/form.js
@@ -1,6 +1,6 @@
 const form = document.getElementsByTagName('form')[0],
-    inputs = document.querySelectorAll('.form-field input, .form-field textarea'),
-    errors = [ nameError, mailError, messageError] = document.getElementsByClassName('error');
+    inputs = [nameInput, emailInput, messageInput] = document.querySelectorAll('.form-field input, .form-field textarea'),
+    errors = [nameError, emailError, messageError] = document.getElementsByClassName('error');
 
 
 Array.from(inputs).forEach(i => {
@@ -21,7 +21,19 @@ Array.from(inputs).forEach(i => {
             parent.classList.add('empty');
         }
 
-        // show error message if field was left empty
+        const nameRegex = new RegExp(/^(\w{1,20}[\s\-]{0,1}\w{0,20})\s*(\w{0,20}[\s\-']{0,1}\w{0,20})$/, 'gi');
+        if (nameRegex.test(nameInput.value) === false) {
+            nameError.className = "error active";
+            nameError.innerHTML = "Please provide correct name";
+        }
+
+        // show error message if email is incorrect
+        if (!emailInput.validity.valid) {
+            emailError.className = "error active";
+            emailError.innerHTML = "Please provide correct email";
+        }
+
+        // show error message if any field was left empty
         if (i.value === '') {
             error.innerHTML = `Please enter your ${i.name}`;
             error.className = "error active";
@@ -35,4 +47,22 @@ Array.from(inputs).forEach(i => {
             }
         });
     });
+});
+
+
+// check input fields for validity
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // if there are empty fields left or if there are any active errors,
+    // show error message
+    const atLeastOneInputEmpty = [...inputs].some(i => i.value === '');
+    const atLeastOneErrorPresent = document.querySelector('.active');
+
+    if (atLeastOneInputEmpty || atLeastOneErrorPresent) {
+        console.log('Form is not validated!');
+        return;
+    }
+
+    console.log('Form is validated!');
 });
